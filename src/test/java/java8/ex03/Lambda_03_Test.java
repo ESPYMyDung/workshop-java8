0a18f9247af8136eec63a0c7bff8a8e87a972262
ESPY My Dung <ngocdiepespy@sfr.fr>
2019-03-26 09:51:48 +0100

    tp java8 lambda ex3

--- src/test/java/java8/ex03/Lambda_03_Test.java
+++ src/test/java/java8/ex03/Lambda_03_Test.java
@@ -5,6 +5,7 @@ import java8.data.Person;
 import org.junit.Test;
 
 import java.util.List;
+//import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -12,31 +13,45 @@ import static org.junit.Assert.*;
 /**
  * Exercice 03 - ForEach
  */
-public class Lambda_03_Test {
+public class Lambda_03_Test
+{
 
-    // tag::PersonProcessor[]
-    interface PersonProcessor {
+    // ----tag::PersonProcessor[]----
+    interface PersonProcessor
+    {
         void process(Person p);
     }
     // end::PersonProcessor[]
 
-    // tag::forEach[]
-    private void forEach(List<Person> source, PersonProcessor processor) {
-       // TOD0
+    // ----tag::forEach[]----
+    private void forEach(List<Person> source, PersonProcessor processor)
+    {
+       // et on met quoi ici?
+    	for (Person indv:source)
+    	{
+    		processor.process(indv);
+    	}
     }
     // end::forEach[]
 
 
-    // tag::test_verify_person[]
+    // ----tag::test_verify_person[]----
     @Test
-    public void test_verify_person() throws Exception {
+    public void test_verify_person() throws Exception
+    {
 
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO vérifier qu'une personne à un prénom qui commence par first
         // TODO vérifier qu'une personne à un nom qui commence par last
         // TODO vérifier qu'une personne à un age > 0
-        PersonProcessor verifyPerson = null;
+        PersonProcessor verifyPerson = p ->
+        { //si le code en plusieurs ligne le metre dans un bloc
+        	assertTrue(p.getAge()>0);
+			assertTrue(p.getFirstname().startsWith("first"));
+			assertTrue(p.getLastname().startsWith("last"));
+		};
+
 
         assertThat(verifyPerson, notNullValue());
 
