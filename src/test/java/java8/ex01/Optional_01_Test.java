1c15528f0e1d503e9303fabe3f1889495405cce7
ESPY My Dung <ngocdiepespy@sfr.fr>
2019-03-26 16:40:48 +0100

    tp java8 optional ex1.1

--- src/test/java/java8/ex01/Optional_01_Test.java
+++ src/test/java/java8/ex01/Optional_01_Test.java
@@ -16,11 +16,20 @@ import static org.junit.Assert.*;
  */
 public class Optional_01_Test {
 
-    class NotFountException extends RuntimeException {}
+    class NotFountException extends RuntimeException
+    {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		
+    }
 
 
     // tag::findMethod[]
-    <T> T find(List<T> list, Predicate<T> predicate) {
+    <T> T find(List<T> list, Predicate<T> predicate)
+    {
         T result = null;
 
         for (T p : list) {
@@ -36,13 +45,14 @@ public class Optional_01_Test {
 
 
     @Test
-    public void test_optional_found() throws Exception {
+    public void test_optional_found() throws Exception
+    {
 
         List<Person> personList = Data.buildPersonList(100);
-
+        
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
         // TODO age == 10
-        Optional<Person> result = null;
+        Optional<Person> result = Optional.of(find(personList, p->p.getAge()==10));
 
         assertThat(result, instanceOf(Optional.class));
         assertThat(result.isPresent(), is(true));
@@ -52,34 +62,37 @@ public class Optional_01_Test {
     }
 
     @Test
-    public void test_optional_notfound() throws Exception {
+    public void test_optional_notfound() throws Exception
+    {
 
 
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
         // TODO age == 400
-        Optional<Person> result = null;
+        Optional<Person> result = Optional.ofNullable(find(personList, p->p.getAge()==400));
 
         assertThat(result, instanceOf(Optional.class));
         assertThat(result.isPresent(), is(false));
     }
 
     @Test(expected = NotFountException.class)
-    public void test_optional_notfound_throw_exception() throws Exception {
+    public void test_optional_notfound_throw_exception() throws Exception
+    {
 
 
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO invoquer la méthode find(List<T> list, Predicate<T> predicate)
         // TODO age == 10 et firstname == "last_10"
-        Optional<Person> result = null;
-
+        Optional<Person> result = Optional.ofNullable(find(personList, p-> p.getAge()==10 && p.getFirstname().equals("last_10")));
+        result.orElseThrow( ()-> new NotFountException());
         // TODO Utiliser la méthode orElseThrow pour déclencher l'exception NotFountException si non trouvé
     }
 
     @Test
-    public void test_optional_notfound_with_default_value() throws Exception {
+    public void test_optional_notfound_with_default_value() throws Exception
+    {
 
 
         List<Person> personList = Data.buildPersonList(100);
